b984c0fcaa46206ab15922a5e3d20d7b50a2543b
Huw-man <hu.newman@berkeley.edu>
2020-07-29 17:22:44 -0700

    initial

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent navMeshAgent;
+    private AudioSource audioSource;
+
+    public AudioClip spawnClip;
+    public AudioClip hitClip;
+    public AudioClip dieClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(spawnClip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public float spawnTime;
+    public GameObject monsterPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        Transform spawn = spawnLocations[Random.Range(0, spawnLocations.Length)];
+        GameObject monster = Instantiate(monsterPrefab, spawn.position, spawn.rotation);
+    }
+}
