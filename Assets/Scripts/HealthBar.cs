53cfef6befbe057b1a3f086dbb15c6e82ed7fc42
Daniel Long He <tcdanielh@berkeley.edu>
2020-08-06 19:17:25 -0700

    Added working health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    private Player target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("ScriptManager").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetHealth(target.getHealth());
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
