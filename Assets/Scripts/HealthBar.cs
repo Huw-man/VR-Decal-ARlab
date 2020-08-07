9e34f8f62c6d47de0545a37d59627b3ef0ece3d0
Daniel Long He <tcdanielh@berkeley.edu>
2020-08-07 16:12:43 -0700

    Updated with healthbar to merge

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
