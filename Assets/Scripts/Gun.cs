35410c22337e6ba730a52624d335066f16d4c569
Daniel Long He <tcdanielh@berkeley.edu>
2020-08-17 17:27:11 -0700

    Image tracked gun on a card, shooting mechanics work

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -24,7 +24,14 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Fire();
+            }
+        }
     }
 
     public void Fire()
