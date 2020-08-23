96d35923ff82d36bac268733838a9285d3486246
Laryn Qi <larynqi@berkeley.edu>
2020-08-23 04:16:12 -0700

    rename BGM

--- Assets/BGM.cs
+++ Assets/BGM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class BGSound : MonoBehaviour
+public class BGM : MonoBehaviour
 {
     private AudioSource audioSource;
     private Object[] tracks;
